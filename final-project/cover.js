8cee787247635097297160ba8a467e5a0b18c7ec
sylscm <chenmias@andrew.cmu.edu>
2024-04-23 12:49:10 -0400

    ye

--- final-project/cover.js
+++ final-project/cover.js
@@ -1,2 +1,40 @@
+const broccoli = document.getElementById("runaway");
+const tomato = document.getElementById("runaway2");
+const egg = document.getElementById("runaway3");
 
+const animateMove = (element, prop, pixels) =>
+  anime({
+    targets: element,
+    [prop]: `${pixels}px`,
+    easing: "easeOutCirc"
+  });
+
+["mouseover", "click"].forEach(function (el) {
+  broccoli.addEventListener(el, function (event) {
+    const top = getRandomNumber(window.innerHeight - this.offsetHeight);
+    const left = getRandomNumber(window.innerWidth - this.offsetWidth);
+    animateMove(this, "left", left).play();
+    animateMove(this, "top", top).play();
+  });
+});
+["mouseover", "click"].forEach(function (el) {
+    tomato.addEventListener(el, function (event) {
+      const top = getRandomNumber(window.innerHeight - this.offsetHeight);
+      const left = getRandomNumber(window.innerWidth - this.offsetWidth);
+      animateMove(this, "left", left).play();
+      animateMove(this, "top", top).play();
+    });
+});
+["mouseover", "click"].forEach(function (el) {
+    egg.addEventListener(el, function (event) {
+      const top = getRandomNumber(window.innerHeight - this.offsetHeight);
+      const left = getRandomNumber(window.innerWidth - this.offsetWidth);
+      animateMove(this, "left", left).play();
+      animateMove(this, "top", top).play();
+    });
+});
+
+const getRandomNumber = (num) => {
+  return Math.floor(Math.random() * (num + 1));
+};
 
